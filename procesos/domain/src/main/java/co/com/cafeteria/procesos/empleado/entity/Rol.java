962379fad1196fce8b97eee8ed86289040a143fb
Juanes07 <juanesdm3@gmail.com>
2022-05-19 14:38:28 -0500

    Entidad Rol
    Se agrega la entidad Rol con su comportamiento agregarRol

---
+++ procesos/domain/src/main/java/co/com/cafeteria/procesos/empleado/entity/Rol.java
@@ -0,0 +1,17 @@
+package co.com.cafeteria.procesos.empleado.entity;
+
+import co.com.cafeteria.procesos.values.NombreRol;
+import co.com.sofka.domain.generic.Entity;
+
+public class Rol extends Entity<RolId> {
+    protected NombreRol nombreRol;
+
+    public Rol(RolId id, NombreRol nombreRol) {
+        super(id);
+        this.nombreRol = nombreRol;
+    }
+
+    public void agregarRol(String nombreRol){
+        this.nombreRol = new NombreRol(nombreRol);
+    }
+}
---
+++ procesos/domain/src/main/java/co/com/cafeteria/procesos/empleado/entity/RolId.java
@@ -0,0 +1,9 @@
+package co.com.cafeteria.procesos.empleado.entity;
+
+import co.com.sofka.domain.generic.Identity;
+
+public class RolId extends Identity {
+    public RolId(String id) {
+        super(id);
+    }
+}
