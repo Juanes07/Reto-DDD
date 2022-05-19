ba8919cf24a176f166249232dd1e9985cc611f73
Juanes07 <juanesdm3@gmail.com>
2022-05-19 18:03:37 -0500

    Agregado ZonaDeTrabajo
    Se completa el agregado ZonaDeTrabajo con sus respectivos comportamientos

--- procesos/domain/src/main/java/co/com/cafeteria/procesos/zonadetrabajo/ZonaDeTrabajo.java
+++ procesos/domain/src/main/java/co/com/cafeteria/procesos/zonadetrabajo/ZonaDeTrabajo.java
@@ -4,6 +4,7 @@ import co.com.cafeteria.procesos.empleado.values.EmpleadoId;
 import co.com.cafeteria.procesos.pedido.values.PedidoId;
 import co.com.cafeteria.procesos.zonadetrabajo.entity.Instrumento;
 import co.com.cafeteria.procesos.zonadetrabajo.entity.MateriaPrima;
+import co.com.cafeteria.procesos.zonadetrabajo.events.ZonaDeTrabajoAgregada;
 import co.com.cafeteria.procesos.zonadetrabajo.value.ZonaDeTrabajoId;
 import co.com.sofka.domain.generic.AggregateEvent;
 
@@ -15,7 +16,30 @@ public class ZonaDeTrabajo extends AggregateEvent<ZonaDeTrabajoId> {
     protected PedidoId pedidoId;
     protected List<MateriaPrima> materiaPrima;
 
+    public ZonaDeTrabajo(ZonaDeTrabajoId id, EmpleadoId empleadoId, Instrumento instrumento, PedidoId pedidoId) {
+        super(id);
+        appendChange(new ZonaDeTrabajoAgregada(empleadoId,pedidoId,instrumento));
+        subscribe(new ZonaDeTrabajoEventChange(this));
+    }
 
+    public ZonaDeTrabajo(ZonaDeTrabajoId zonaDeTrabajoId){
+        super(zonaDeTrabajoId);
+        subscribe(new ZonaDeTrabajoEventChange(this));
+    }
 
+    public EmpleadoId EmpleadoId() {
+        return empleadoId;
+    }
 
+    public Instrumento Instrumento() {
+        return instrumento;
+    }
+
+    public PedidoId PedidoId() {
+        return pedidoId;
+    }
+
+    public List<MateriaPrima> MateriaPrima() {
+        return materiaPrima;
+    }
 }
---
+++ procesos/domain/src/main/java/co/com/cafeteria/procesos/zonadetrabajo/ZonaDeTrabajoEventChange.java
@@ -0,0 +1,17 @@
+package co.com.cafeteria.procesos.zonadetrabajo;
+
+import co.com.cafeteria.procesos.zonadetrabajo.events.ZonaDeTrabajoAgregada;
+import co.com.sofka.domain.generic.EventChange;
+
+import java.util.ArrayList;
+
+public class ZonaDeTrabajoEventChange extends EventChange {
+    public ZonaDeTrabajoEventChange(ZonaDeTrabajo zonaDeTrabajo){
+        apply((ZonaDeTrabajoAgregada event)->{
+            zonaDeTrabajo.empleadoId = event.EmpleadoId();
+            zonaDeTrabajo.instrumento = event.Instrumento();
+            zonaDeTrabajo.pedidoId = event.PedidoId();
+            zonaDeTrabajo.materiaPrima = new ArrayList<>();
+        });
+    }
+}
